7b4f668e464e7bae6c95ab43094833088efc6414
chessai <chessai1996@gmail.com>
2019-06-03 09:17:43 -0400

    remove a bunch of unused scripts

--- mk.sh
+++
@@ -1,2 +0,0 @@
-cabal configure  --package-db=../../.cabal-sandbox/x86_64-osx-ghc-7.10.2-packages.conf.d/ --enable-tests
-
--- nix-ci-ish.sh
+++
@@ -1,6 +0,0 @@
-nix-shell -I nixpkgs=https://github.com/nixos/nixpkgs-channels/archive/nixos-17.09.tar.gz -p haskell.compiler.ghc7103 --run "cabal new-build"
-nix-shell -I nixpkgs=https://github.com/nixos/nixpkgs-channels/archive/nixos-17.09.tar.gz -p haskell.compiler.ghc802 --run "cabal new-build"
-nix-shell -I nixpkgs=https://github.com/nixos/nixpkgs-channels/archive/nixos-18.09.tar.gz -p haskell.compiler.ghc822 --run "cabal new-build"
-nix-shell -I nixpkgs=https://github.com/nixos/nixpkgs-channels/archive/nixos-18.09.tar.gz -p haskell.compiler.ghc844 --run "cabal new-build"
-nix-shell -I nixpkgs=https://github.com/nixos/nixpkgs-channels/archive/nixos-18.09.tar.gz -p haskell.compiler.ghc862 --run "cabal new-build"
-
